fc274d50ebf71b10aa4cf0a05457addebb086c60
virajhmehta <virajmehta2499@gmail.com>
2024-01-01 17:30:05 +0530

    Todo Application React

--- 10todoContextLocal/src/App.jsx
+++ 10todoContextLocal/src/App.jsx
@@ -1,16 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { TodoProvider } from "./contexts";
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
 
+  // we have compare the ids and if it is matchig with current change to prev
+  // or else keep prev
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
+
+  // with delete we mostly use filter
+  // so mostly filter use with matching the id so we have implemeted
+  // such a way that it filters out those which  is not matching
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo !== id));
+  };
+
+  // for toggleComplete we have go inside the todos array to change the
+  // status of complete to toggle it
+  // first we have to find with id
+  // if it is matching then we will destructure the obj and toggle the value
+  // if is is not matching then we will keep the prev value
+
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
   return (
-    <>
-      <h1>Hello World</h1> 
-    </>
-  )
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
+    >
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
+  );
 }
 
-export default App
+export default App;
--- 10todoContextLocal/src/contexts/TodoContext.js
+++ 10todoContextLocal/src/contexts/TodoContext.js
@@ -1,5 +1,18 @@
 import { createContext, useContext } from "react";
-export default TodoContext = createContext({});
+
+export const TodoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      todo: " Todo msg",
+      completed: false,
+    },
+  ],
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {},
+});
 
 export const useTodo = () => {
   return useContext(TodoContext);
